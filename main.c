4d44e75f2f2183aff87f428b133af8c2d3769d52
PolloDiablo <jer.kroeker@gmail.com>
2015-06-19 18:16:27 -0700

    Hello World

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+SENG 440
+Cryptography Project
+
+By: Jeremy Kroeker and Daniel Faulkner
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv){
+	printf("Hello World\n");
+	printf("Hello World?\n");
+	printf("Hello World!\n");
+	
+	exit(0);
+}
+
