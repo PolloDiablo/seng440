7008d08a2b564a9897932b64d810b6d4060e7d02
maxentropic <danielafaulkner@gmail.com>
2015-07-26 15:32:31 -0700

    Code cleaning and commenting. Upgraded the modulus to an 8 byte number.

--- main.c
+++ main.c
@@ -9,17 +9,15 @@
 #include <string.h>
 #include <stdio.h>
 
-#define n 64
 #define m 64
-//39
 #define BYTE_COUNT 8
 
 typedef unsigned char BYTE;
 
 void encrypt(unsigned char* out, unsigned const char* input, unsigned const char* pubkey, unsigned const char* modulus);
 void decrypt(unsigned char* out, unsigned const char* input, unsigned const char* privkey, unsigned const char* modulus);
-void exponentiate(unsigned char* out, unsigned const char* input, unsigned const char* exponent, unsigned const char* modulus);
-void montgomerize(unsigned char* out, unsigned const char* x, unsigned const char* y, unsigned const char* modulus);
+void mme(unsigned char* out, unsigned const char* input, unsigned const char* exponent, unsigned const char* modulus);
+void mmm(unsigned char* out, unsigned const char* x, unsigned const char* y, unsigned const char* modulus);
 
 void multiply_wc(unsigned char* out, unsigned const char* a, unsigned const char* b);
 unsigned char add_wc(unsigned char* out, unsigned const char* a, unsigned const char* b);
@@ -27,49 +25,59 @@ unsigned char subtract_wc(unsigned char* out, unsigned const char* a, unsigned c
 void rightshift_wc(unsigned char* a);
 int gte(unsigned const char* a, unsigned const char* b);
 
-// TEMP functions
+// TEMP functions --------------------------
 void print_array(unsigned const char* arr);
 void mont_test();
 void full_test();
 
 int DEBUG_LINES = 0;
+// -----------------------------------------
+
+unsigned const char P[BYTE_COUNT] = { 0x00, 0x00, 0x00, 0x02, 0xD8, 0x2F, 0xA7, 0x6D };     // 12216936301
+unsigned const char Q[BYTE_COUNT] = { 0x00, 0x00, 0x00, 0x00, 0x14, 0xD5, 0x4E, 0x53 };     // 349523539
+unsigned const char M[BYTE_COUNT] = { 0x3B, 0x42, 0x77, 0x85, 0x6E, 0x27, 0x7E, 0x57 };     // 4270106811663089239
+unsigned const char E[BYTE_COUNT] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x95 };     // 149
+unsigned const char D[BYTE_COUNT] = { 0x12, 0xB1, 0x50, 0xA6, 0x96, 0xB1, 0x8D, 0x05 };     // 134694643998500549
+unsigned const char Nr[BYTE_COUNT] = { 0x0B, 0xC8, 0xEC, 0x02, 0xEF, 0xBA, 0x65, 0x3B };    // 849188027115398459
+
 //
-
-unsigned const char P[BYTE_COUNT] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x05, 0xF3, 0xBD };
-unsigned const char Q[BYTE_COUNT] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x0E, 0x0E, 0xCD };
-unsigned const char M[BYTE_COUNT] = { 0x00, 0x00, 0x00, 0x53, 0xAC, 0x6E, 0x84, 0x59 };
-unsigned const char E[BYTE_COUNT] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x21 };
-//unsigned const char D[BYTE_COUNT] = { 0x00, 0x00, 0x00, 0x05, 0x12, 0x35, 0x3E, 0x53 };
-unsigned const char D[BYTE_COUNT] = { 0x00, 0x00, 0x00, 0x51, 0x23, 0x40, 0x7D, 0xE1 };
-unsigned const char Nr[BYTE_COUNT] = { 0x00, 0x00, 0x00, 0x40, 0x45, 0xD1, 0x59, 0xFA };
-
-int main(int argc, char **argv) {
-    //mont_test();
+// Runs the full test with a default value and exits.
+//
+int main() {
     full_test();
     exit(0);
 }
 
+//
+// For testing - prints the octets of a char array. Assumes the array contains
+// exactly BYTE_COUNT octets.
+//
 void print_array(unsigned const char* arr) {
     int i;
 
     for (i = 0; i < BYTE_COUNT; i++) {
         printf("%02x ", arr[i]);
-        //printf("%c ", arr[i]);
     }
     printf("\n");
 }
 
+//
+// For testing - runs the mmm funtion using the x, y, and modulus values
+// given in the example in the slides. Prints the resulting array.
+//
 void mont_test() {
     unsigned char output[BYTE_COUNT];
     unsigned char x[BYTE_COUNT] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x11 };
     unsigned char y[BYTE_COUNT] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16 };
     unsigned char mod[BYTE_COUNT] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x17 };
-    montgomerize(output, x, y, mod); 
+    mmm(output, x, y, mod); 
     print_array(output);
 }
 
+//
+// For testing - encrypts the test input, then decrypts it. Prints both char arrays.
+//
 void full_test() {
-    //unsigned char* teststring = "hi";
     unsigned char teststring[BYTE_COUNT] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x48, 0x49 };
     unsigned char output[BYTE_COUNT];
     unsigned char next_output[BYTE_COUNT];
@@ -77,50 +85,87 @@ void full_test() {
     decrypt(next_output, output, D, M);
     print_array(output);
     print_array(next_output);
-    //printf("%.*s\n", BYTE_COUNT, next_output);
 }
 
+//
+// Encrypts the input by calling the montgomery modular exponentiation function with the public key.
+// All char arrays must be BYTE_COUNT bytes long.
+//
+//   out:       The char array the output will be written to.
+//   input:     The value to encrypt.
+//   pubkey:    The public key to encrypt with.
+//   modulus:   The shared modulus to encrypt with.
+//
 void encrypt(unsigned char* out, unsigned const char* input, unsigned const char* pubkey, unsigned const char* modulus) {
-    exponentiate(out, input, pubkey, modulus);
+    mme(out, input, pubkey, modulus);
 }
 
+//
+// Decrypts the input by calling the montgomery modular exponentiation function with the private key.
+// All char arrays must be BYTE_COUNT bytes long.
+//
+//   out:       The char array the output will be written to.
+//   input:     The cypher to decrypt.
+//   privkey:   The private key to decrypt with.
+//   modulus:   The shared modulus to decrypt with.
+//
 void decrypt(unsigned char* out, unsigned const char* input, unsigned const char* privkey, unsigned const char* modulus) {
-    exponentiate(out, input, privkey, modulus);
+    mme(out, input, privkey, modulus);
 }
 
-void exponentiate(unsigned char* out, unsigned const char* input, unsigned const char* exponent, unsigned const char* modulus) {
+//
+// Runs montgomery modular exponentiation function on the input, using the exponent and modulus provided and writes
+// the result to the output char array. All char arrays must be BYTE_COUNT long.
+//
+// This function calculates (input ^ exponent) MOD modulus.
+//
+//   out:       The char array the output will be written to.
+//   input:     The base of the mathematical function.
+//   exponent:  The exponent of the mathematical function.
+//   modulus:   The modulus of the mathematical function.
+//
+void mme(unsigned char* out, unsigned const char* input, unsigned const char* exponent, unsigned const char* modulus) {
     unsigned char One[BYTE_COUNT] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01 };
     unsigned char Z[BYTE_COUNT];
     unsigned char P_loc[BYTE_COUNT];
     unsigned char P_next[BYTE_COUNT];
     unsigned char E_loc[BYTE_COUNT];
-
-    montgomerize(Z, One, Nr, modulus);
-    montgomerize(P_loc, Nr, input, modulus);
-
-    //DEBUG_LINES = 0;
-    
-    //memcpy(P_loc, input, BYTE_COUNT);
-    memcpy(E_loc, exponent, BYTE_COUNT);
-
     int i;
+
+    // Pre-scale the initial values.
+    mmm(Z, One, Nr, modulus);
+    mmm(P_loc, Nr, input, modulus);
+
+    memcpy(E_loc, exponent, BYTE_COUNT);
     
-    for (i = 0; i < n; i++) {
-        montgomerize(P_next, P_loc, P_loc, modulus);
+    for (i = 0; i < m; i++) {
+        mmm(P_next, P_loc, P_loc, modulus);
         if (E_loc[7] & 0x01) {
-            montgomerize(Z, Z, P_loc, modulus);
+            mmm(Z, Z, P_loc, modulus);
         } else {
-            // Do nothing!
+            // Zi+1 gets the value of Zi; this is accomplished by leaving Z as it is.
         }
 
         memcpy(P_loc, P_next, BYTE_COUNT);
         rightshift_wc(E_loc);
     }
-    montgomerize(out, One, Z, modulus);
-    //memcpy(out, Z, BYTE_COUNT);    
+
+    // Remove the R ^ -1 factor from the result.
+    mmm(out, One, Z, modulus);
 }
 
-void montgomerize(unsigned char* out, unsigned const char* x, unsigned const char* y, unsigned const char* modulus){
+//
+// Runs montgomery modular multiplication function on the input, using the x and y values with the modulus provided.
+// Writes the result to the output char array. All char arrays must be BYTE_COUNT long.
+//
+// This function calculates (x * y * R ^ -1 MOD modulus), where R ^ -1 is (2 ^ 2m) MOD modulus.
+//
+//    out:      The char array the output will be written to.
+//    x:        One of the two multiplicands of the mathematical function.
+//    y:        One of the two multiplicands of the mathematical function.
+//    modulus:  The modulus of the mathematical function.
+//
+void mmm(unsigned char* out, unsigned const char* x, unsigned const char* y, unsigned const char* modulus){
     unsigned char T[BYTE_COUNT] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
     unsigned char x_loc[BYTE_COUNT];
     
@@ -129,44 +174,38 @@ void montgomerize(unsigned char* out, unsigned const char* x, unsigned const cha
     int i;
     char temp_bit;
     char x0; // register
-
-    if (DEBUG_LINES) {
-        print_array(x_loc);
-        print_array(y);
-        print_array(modulus);
-    }
+    unsigned char overflow;
 
     for(i = 0 ; i < m ; i++){
-        if (DEBUG_LINES) { printf("MMM loop iteration number %i:\n", i); }
         x0 = x_loc[7] & 0x01;
         temp_bit = ((T[7] & 0x01) + (x0 & y[7])) & 0x01;
-        if (DEBUG_LINES) { printf("T0: %02x | X(i): %02x | Y(0): %02x | result: %02x\n", T[7] & 0x01, x0, y[7], temp_bit); }
+
+        // Adds values to the product based on the flags. Records overflow if it occurs.
         if (x0) {
-            if (add_wc(T, T, y)) {
-                printf("Add overflow with x0\n");
-            }
+            overflow = add_wc(T, T, y);
         }
         if (temp_bit) {
-            //printf("t=%02x\n", temp_bit);
-            if (add_wc(T, T, modulus)) {
-                printf("Add overflow with temp_bit\n");
-            }
+            overflow += add_wc(T, T, modulus);
         }
+        
         rightshift_wc(T);
+        T[0] |= (overflow << 7);
+        overflow = 0x00;
         rightshift_wc(x_loc);
-        if (DEBUG_LINES) { print_array(T); }
     }
 
-    /*if(gte(T,M)){
-        subtract_wc(T, T, M);
-    }*/
     memcpy(out, T, BYTE_COUNT);
 }
 
-void multiply_wc(unsigned char* out, unsigned const char* a, unsigned const char* b){
-    //TODO
-}
-
+//
+// Adds two words together and writes to the output char array. All char arrays must be BYTE_COUNT long.
+//
+//    out:      The char array that the output will be written to.
+//    a:        One of the two summands.
+//    b:        One of the two summands.
+//
+//    returns:  1 if there was overflow; 0 otherwise.
+//
 unsigned char add_wc(unsigned char* out, unsigned const char* a, unsigned const char* b){
     int i;
     unsigned char overflow = 0;
@@ -189,26 +228,11 @@ unsigned char add_wc(unsigned char* out, unsigned const char* a, unsigned const 
     return overflow;
 }
 
-unsigned char subtract_wc(unsigned char* out, unsigned const char* a, unsigned const char* b){
-    int i;
-    unsigned char borrow = 0;
-    unsigned char gt;
-    unsigned char msb_a;
-    unsigned char msb_b;
-
-    for (i = BYTE_COUNT - 1; i >= 0; i--) {
-        gt = (unsigned char)(b[i] > a[i]);
-        out[i] = a[i] - b[i];
-        msb_a = out[i] & 0x80;
-        out[i] -= borrow;
-        msb_b = out[i] & 0x80;
-
-        borrow = gt | (((0x80 ^ msb_a) & msb_b) >> 7);
-    }
-
-    return borrow;
-}
-
+//
+// Shifts the given word right by one. Performed in place. The char array must be BYTE_COUNT long.
+//
+//   a:     The char array bit shifted in place one to the right.
+//
 void rightshift_wc(unsigned char* a){
     int i;
     char carry = 0x00;
@@ -220,23 +244,3 @@ void rightshift_wc(unsigned char* a){
     }
 }
 
-//
-// Checks if char array a is greater than or equal to char array b.
-// 
-// Return: 1 if a >= b; 0 otherwise
-//
-int gte(unsigned const char* a, unsigned const char* b){
-    int is_gte = 1;
-    int i = 0;
-
-    while (i < BYTE_COUNT) {
-        if ((a[i] & b[i]) != a[i]) {
-            is_gte = a[i] > b[i];
-            break;
-        }
-
-        i++;
-    }
-
-    return is_gte;
-}
